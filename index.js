7b99dcc708683970a07534a98f36ac4c658e229f
Ernie Hsiung <e@erniehsiung.com>
2022-03-29 13:05:19 -0400

    undo bug

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require('express');
 const warehouseRoutes = require('./routes/warehouses');
 const inventoryRoutes = require('./routes/inventories');
 const morgan = require('morgan');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/v1/warehouses', warehouseRoutes);
